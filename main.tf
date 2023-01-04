57cc66be1b9c667451f25fe966833f55beb8712e
pmoghle <110378952+pmoghle@users.noreply.github.com>
2023-01-04 17:31:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,3 +10,6 @@ resource "aws_s3_bucket_public_access_block" "test-bucket" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
+[testing]
+aws_access_key_id = AKIAQPEKF2BZAWQMHXIZ
+aws_secret_access_key = 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
